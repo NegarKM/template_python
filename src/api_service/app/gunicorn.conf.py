be04cd487d3ba13bb6a8a74345e7ff121d7f585e
Negar Kermanshahimonfared <negar@Negars-MacBook-Pro.local>
2024-09-25 10:05:58 -0400

    gunicorn.conf

--- src/api_service/app/gunicorn.conf.py
+++ src/api_service/app/gunicorn.conf.py
@@ -1,18 +1,5 @@
 import os
-from multiprocessing import cpu_count
-from typing import Any
 
 bind = f"0.0.0.0:{os.getenv('BIND_PORT', '5000')}"
-workers = min(cpu_count() * 2 + 1, 3)
+workers = 1
 timeout = int(os.getenv("GUNICORN_TIMEOUT", "90"))
-
-
-def child_exit(server: Any, worker: Any) -> None:  # pylint: disable=unused-argument
-    """Called as soon as a worker ends. Pushes the PID of said worker to the
-    Prometheus Metric Exporter.
-
-    Args:
-        server: the Arbiter that controls all Gunicorns in the master thread
-        worker: Worker that has just exited.
-    """
-    pass
